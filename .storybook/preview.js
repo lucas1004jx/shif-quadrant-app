a12baf7dd48aa19054b147da571adb0183f4d182
lucas1004jx <lucas1004jx@gmail.com>
2021-01-13 11:52:44 +0100

    add material UI theme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,3 +5,10 @@ import theme from '../src/theme';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+
+ export const decorators = [ (story) => {
+    return (
+     <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+    )
+}]
